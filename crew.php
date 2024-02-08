cd3aca512c2239087f862080d48e3f79ca36ea26
yamsmdj <yamine.mdj@gmail.com>
2024-02-08 09:37:29 +0100

    new crew add

--- crew.php
+++ crew.php
@@ -5,14 +5,14 @@ $crews = allCrew($db);
 
 ?>
 
-<div class="d-flex flex-wrap justify-content-around align-items-center h-100 w-50 m-auto ">
+<div class="d-flex flex-wrap row-gap-4 justify-content-around align-items-center h-100 w-50 m-auto ">
     <?php foreach ($crews as $crew) :?>
-        <div class="card bg-dark " style="width: 20rem; height:18rem;">
-      <div class="card-body overflow-hidden">
-        <a class="px-3" href="/mycrew.php?id=<?= $crew['id'] ?> "><img src="<?= $crew['pathimg'] ?>" class="h-100 img-fluid " alt="<?= $crew['name'] ?>" ></a>
+        <div class="card bg-dark " style="width: 12rem; height:12rem;">
+      <div class="card-body overflow-hidden p-0">
+        <a  href="/mycrew.php?id=<?= $crew['id'] ?> "><img src="<?= $crew['pathimg'] ?>" class="h-100 img-fluid " alt="<?= $crew['name'] ?>" ></a>
       </div>
     </div>
     <?php endforeach ;
-require_once __DIR__ . '/function/crud.fn.php' ; ?>
+// require_once __DIR__ . '/function/crud.fn.php' ; ?>
 <?php 
 require_once __DIR__ . '/utilities/footer.php'; ?>
